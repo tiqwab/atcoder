815d6e04a58357ab0f8d3ffd0191d3b0fe3817bb
tiqwab <tiqwab.ch90@gmail.com>
2020-03-02 17:49:15 +0900

    Add union_find

---
+++ lib/union_find.cpp
@@ -0,0 +1,90 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+
+template<class T>
+inline bool chmax(T &a, T b) {
+    if(a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+template<class T>
+inline bool chmin(T &a, T b) {
+    if(a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+/*
+ * ref. [Union-Find](https://www.slideshare.net/chokudai/union-find-49066733)
+ * ref. [AtCoder ABC 157 - AtCoder Live](https://www.youtube.com/watch?v=TdR816rqc3s)
+ */
+class UnionFind {
+    public:
+        UnionFind(int n = 0): d(n, -1) {}
+
+        // 0-index, [0, n) in other words
+        int find(int x) {
+            if (d[x] < 0) return x;
+            return d[x] = find(d[x]);
+        }
+
+        // Return false if x and y are alreday in the same group, otherwise return true.
+        // 0-index, [0, n) in other words
+        bool unite(int x, int y) {
+            x = find(x);
+            y = find(y);
+            if (x == y) return false;
+            if (d[x] > d[y]) swap(x, y);
+            d[x] += d[y];
+            d[y] = x;
+            return true;
+        }
+
+        // 0-index, [0, n) in other words
+        bool same(int x, int y) {
+            return find(x) == find(y);
+        }
+
+        // 0-index, [0, n) in other words
+        int size(int x) {
+            return -d[find(x)];
+        }
+
+    private:
+        vector<int> d;
+};
+
+// AtCoder Typical Contests001 B: Union Find
+// https://atc001.contest.atcoder.jp/tasks/unionfind_a
+int main(void) {
+    int N, Q;
+    cin >> N >> Q;
+
+    UnionFind uf(N);
+
+    for (int i = 0; i < Q; i++) {
+        int P, A, B;
+        cin >> P >> A >> B; // A, B is 0-index
+        if (P == 0) {
+            uf.unite(A, B);
+        } else {
+            if (uf.same(A, B)) {
+                cout << "Yes" << endl;
+            } else {
+                cout << "No" << endl;
+            }
+        }
+    }
+
+    return 0;
+}

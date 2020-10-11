eb7f6bbabc0bd869602d61a53ea01c13098aef34
tiqwab <tiqwab.ch90@gmail.com>
2020-10-11 12:20:32 +0900

    abl (a-d)

---
+++ abl/a/main.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <vector>
+
+#include <limits.h>
+
+using namespace std;
+
+typedef long long ll;
+
+template<class T>
+inline bool chmax(T &a, T b) {
+    if(a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+template<class T>
+inline bool chmin(T &a, T b) {
+    if(a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+int main(void) {
+    int K;
+    cin >> K;
+    for (int i = 0; i < K; i++) {
+        cout << "ACL";
+    }
+    cout << endl;
+    return 0;
+}
---
+++ abl/d/main.cpp
@@ -0,0 +1,116 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <vector>
+
+#include <limits.h>
+
+using namespace std;
+
+typedef long long ll;
+
+template<class T>
+inline bool chmax(T &a, T b) {
+    if(a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+template<class T>
+inline bool chmin(T &a, T b) {
+    if(a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const int INF = (int) ((1LL << 31) - 1);
+
+// ref. arihon 3-3
+// E: type of data, R: type of query result
+template<typename E, typename R>
+class SegmentTree {
+    public:
+        SegmentTree(const int _n, const E inf, R (*f)(E, E)): f(f), inf(inf) {
+            n = 1;
+            while (n < _n) n *= 2;
+            data.resize(2 * n, inf);
+        }
+
+        // update data[k] with a
+        // 0-index, [0, _n) in other words
+        // time complexity: O(n)
+        void update(int k, E a) {
+            k += n - 1;
+            data[k] = a;
+            while (k > 0) {
+                k = (k - 1) / 2;
+                data[k] = (*f)(data[k * 2 + 1], data[k * 2 + 2]);
+            }
+        }
+
+        // query value from [a, b)
+        // 0-index, [0, _n) in other words
+        // time complexity: O(n)
+        R query(int a, int b) {
+            return query(a, b, 0, 0, n);
+        }
+
+    private:
+        int n;
+        R (*f)(E, E);
+        const E inf;
+        vector<E> data;
+
+        R query(int a, int b, int k, int l, int r) {
+            // if [a, b) intersect [l, r) = empty
+            if (r <= a || b <= l) {
+                return inf;
+            }
+
+            // if [a, b) includes [l, r)
+            if (a <= l && r <= b) {
+                return data[k];
+            } else {
+                R vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
+                R vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
+                return (*f)(vl, vr);
+            }
+        }
+};
+
+const int MAX_N = 300000;
+const int MAX_A = 300000;
+const int MAX_K = 300000;
+
+SegmentTree<int, int> *st;
+
+int main(void) {
+    auto f = [](int x, int y) {
+        return max(x, y);
+    };
+    st = new SegmentTree<int, int>(MAX_A + MAX_K + 1, 0, f);
+
+    int N, K;
+    cin >> N >> K;
+    
+    for (int i = 0; i < N; i++) {
+        int a;
+        cin >> a;
+
+        int s = st->query(a - K, a + K + 1);
+
+        st->update(a, s + 1);
+    }
+
+    cout << st->query(0, MAX_A + MAX_K) << endl;
+
+    delete st;
+    return 0;
+}
